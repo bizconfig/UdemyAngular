13cb016998cc2a1e2df096183f0bf6fa734ce5ef
bizconfig <rakesh@users.noreply.github.com>
2018-06-26 00:08:28 +0530

    Ingredient added to shopping list
    Recipe detail and shopping list ingredient added.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Ingredient} from '../shared/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+ingredients:Ingredient[]=[
+  new Ingredient('Lemon',10),  
+  new Ingredient('Mango',5)
+];
   constructor() { }
 
   ngOnInit() {
